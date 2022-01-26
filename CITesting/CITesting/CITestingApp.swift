e620b052546b11f10b3c642077d7dea82e5f41a9
Andreas Maerki <andreas.maerki@valora.com>
2022-01-26 13:47:21 +0100

    Initialising Xcode project

---
+++ CITesting/CITesting/CITestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CITestingApp.swift
+//  CITesting
+//
+//  Created by Andreas Maerki on 26.01.22.
+//
+
+import SwiftUI
+
+@main
+struct CITestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
